b2fb549920995b8bc834ca62b039f6ba1c170661
HenryBrown123 <henry.e.brown@icloud.com>
2019-08-18 02:51:00 +0100

    Added ability to show colors using button

--- client/src/pages/Game.jsx
+++ client/src/pages/Game.jsx
@@ -16,6 +16,7 @@ const AppContainer= styled.div.attrs({
 
 const Tile= styled.div.attrs({
     className: 'tile',
+
 })`
       width: 100%;
       display: inline-block;
@@ -63,11 +64,16 @@ const ClockContainer = styled.div.attrs({
 class Game extends Component{
     constructor(props) {
         super(props)
+        var emptyTile = {word:"",color:"",revealed:"false"}
+        var emptyGame = []
+        for(var i=0; i<12; i++){
+            emptyGame.push(emptyTile)
+        }
         this.state = {
             name:"World",
             tileColors:["grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey","grey"], 
             words:[{word:""},{word:""},{word:""},{word:""},{word:""},{word:""},{word:""},{word:""},{word:""},{word:""},{word:""},{word:""}],
-            game:[{word:"",color:""},{word:"",color:""},{word:"",color:""},{word:"",color:""},{word:"",color:""},{word:"",color:""},{word:"",color:""},{word:"",color:""},{word:"",color:""},{word:"",color:""},{word:"",color:""},{word:"",color:""}],
+            game: emptyGame,
             gameStarted: true,
             redScore:0,
             greenScore:0,
@@ -82,6 +88,31 @@ class Game extends Component{
         }
     }
 
+    hideColors = () =>{
+        var hiddenColors = []
+        for(var index in this.state.game){
+            if(!this.state.game[index].revealed){
+                hiddenColors.push("grey")
+            }else{
+                hiddenColors.push(this.state.game[index].color)
+            }
+        }
+        this.setState({
+            tileColors : hiddenColors
+        })
+    }
+
+    revealColors = () => {
+        var realColors = []
+        for(var index in this.state.game){
+            realColors.push(this.state.game[index].color)
+            console.log(realColors[index])
+        }
+        this.setState({
+            tileColors: realColors
+        })
+    }
+
     nextTurn = (prevTurn) => {
         const nextTurn = prevTurn === "red" ? "green" : "red"
         console.log(nextTurn)
@@ -92,12 +123,17 @@ class Game extends Component{
         })
     }
 
-    showColor = (tile) => {
+    revealColor = (tile) => {
         var colorsToShow = this.state.tileColors
         var tileColor = this.state.game[tile].color
         colorsToShow[tile]= tileColor
+
+        var gameUpdate = this.state.game
+        gameUpdate[tile].revealed=true
+
         var newRedScore = this.state.redScore
         var newGreenScore = this.state.greenScore
+
         var gameOver = this.state.gameOver
         if(tileColor==="red"){
             newRedScore+=1
@@ -146,22 +182,22 @@ class Game extends Component{
                     <Container>
                         <Column> 
                             <Row> 
-                               <Column col xl="3" lg="3" md="3" sm="3"><Tile style={{background: this.state.tileColors[0]}} onClick={() => this.showColor(0)}><Word>{game[0]["word"]}</Word></Tile></Column> 
-                               <Column col xl="3" lg="3" md="3" sm="3"><Tile style={{background: this.state.tileColors[1]}} onClick={() => this.showColor(1)}><Word>{game[1]["word"]}</Word></Tile></Column> 
-                               <Column col xl="3" lg="3" md="3" sm="3"><Tile style={{background: this.state.tileColors[2]}} onClick={() => this.showColor(2)}><Word>{game[2]["word"]}</Word></Tile></Column> 
-                               <Column col xl="3" lg="3" md="3" sm="3"><Tile style={{background: this.state.tileColors[3]}} onClick={() => this.showColor(3)}><Word>{game[3]["word"]}</Word></Tile></Column> 
+                               <Column col xl="3" lg="3" md="3" sm="3"><Tile revealed={this.state.game[0]["revealed"]} style={{background: this.state.tileColors[0]}} onClick={() => this.revealColor(0)}><Word>{game[0]["word"]}</Word></Tile></Column> 
+                               <Column col xl="3" lg="3" md="3" sm="3"><Tile revealed={this.state.game[1]["revealed"]} style={{background: this.state.tileColors[1]}} onClick={() => this.revealColor(1)}><Word>{game[1]["word"]}</Word></Tile></Column> 
+                               <Column col xl="3" lg="3" md="3" sm="3"><Tile revealed={this.state.game[2]["revealed"]} style={{background: this.state.tileColors[2]}} onClick={() => this.revealColor(2)}><Word>{game[2]["word"]}</Word></Tile></Column> 
+                               <Column col xl="3" lg="3" md="3" sm="3"><Tile revealed={this.state.game[3]["revealed"]} style={{background: this.state.tileColors[3]}} onClick={() => this.revealColor(3)}><Word>{game[3]["word"]}</Word></Tile></Column> 
                             </Row>
                             <Row>
-                               <Column col xl="3" lg="3" md="3" sm="3"><Tile style={{background: this.state.tileColors[4]}} onClick={() => this.showColor(4)}><Word>{game[4]["word"]}</Word></Tile></Column> 
-                               <Column col xl="3" lg="3" md="3" sm="3"><Tile style={{background: this.state.tileColors[5]}} onClick={() => this.showColor(5)}><Word>{game[5]["word"]}</Word></Tile></Column> 
-                               <Column col xl="3" lg="3" md="3" sm="3"><Tile style={{background: this.state.tileColors[6]}} onClick={() => this.showColor(6)}><Word>{game[6]["word"]}</Word></Tile></Column> 
-                               <Column col xl="3" lg="3" md="3" sm="3"><Tile style={{background: this.state.tileColors[7]}} onClick={() => this.showColor(7)}><Word>{game[7]["word"]}</Word></Tile></Column> 
+                               <Column col xl="3" lg="3" md="3" sm="3"><Tile revealed={this.state.game[4]["revealed"]} style={{background: this.state.tileColors[4]}} onClick={() => this.revealColor(4)}><Word>{game[4]["word"]}</Word></Tile></Column> 
+                               <Column col xl="3" lg="3" md="3" sm="3"><Tile revealed={this.state.game[5]["revealed"]} style={{background: this.state.tileColors[5]}} onClick={() => this.revealColor(5)}><Word>{game[5]["word"]}</Word></Tile></Column> 
+                               <Column col xl="3" lg="3" md="3" sm="3"><Tile revealed={this.state.game[6]["revealed"]} style={{background: this.state.tileColors[6]}} onClick={() => this.revealColor(6)}><Word>{game[6]["word"]}</Word></Tile></Column> 
+                               <Column col xl="3" lg="3" md="3" sm="3"><Tile revealed={this.state.game[7]["revealed"]} style={{background: this.state.tileColors[7]}} onClick={() => this.revealColor(7)}><Word>{game[7]["word"]}</Word></Tile></Column> 
                             </Row>
                             <Row>
-                               <Column col xl="3" lg="3" md="3" sm="3"><Tile style={{background: this.state.tileColors[8]}} onClick={() => this.showColor(8)}><Word>{game[8]["word"]}</Word></Tile></Column> 
-                               <Column col xl="3" lg="3" md="3" sm="3"><Tile style={{background: this.state.tileColors[9]}} onClick={() => this.showColor(9)}><Word>{game[9]["word"]}</Word></Tile></Column> 
-                               <Column col xl="3" lg="3" md="3" sm="3"><Tile style={{background: this.state.tileColors[10]}} onClick={() => this.showColor(10)}><Word>{game[10]["word"]}</Word></Tile></Column> 
-                               <Column col xl="3" lg="3" md="3" sm="3"><Tile style={{background: this.state.tileColors[11]}} onClick={() => this.showColor(11)}><Word>{game[11]["word"]}</Word></Tile></Column> 
+                               <Column col xl="3" lg="3" md="3" sm="3"><Tile revealed={this.state.game[8]["revealed"]} style={{background: this.state.tileColors[8]}} onClick={() => this.revealColor(8)}><Word>{game[8]["word"]}</Word></Tile></Column> 
+                               <Column col xl="3" lg="3" md="3" sm="3"><Tile revealed={this.state.game[9]["revealed"]} style={{background: this.state.tileColors[9]}} onClick={() => this.revealColor(9)}><Word>{game[9]["word"]}</Word></Tile></Column> 
+                               <Column col xl="3" lg="3" md="3" sm="3"><Tile revealed={this.state.game[10]["revealed"]} style={{background: this.state.tileColors[10]}} onClick={() => this.revealColor(10)}><Word>{game[10]["word"]}</Word></Tile></Column> 
+                               <Column col xl="3" lg="3" md="3" sm="3"><Tile revealed={this.state.game[11]["revealed"]} style={{background: this.state.tileColors[11]}} onClick={() => this.revealColor(11)}><Word>{game[11]["word"]}</Word></Tile></Column> 
                             </Row>
                         </Column>
                     </Container>
@@ -181,6 +217,12 @@ class Game extends Component{
                             </Column>
                             <Column col xl="3" lg="3" md="3" sm="3">
                                 <Button variant="dark" onClick={this.startGame}>New Game</Button>
+                            </Column>
+                            <Column>
+                                <Button variant="dark" onClick={this.revealColors}>Show Colors</Button>
+                            </Column>
+                            <Column>
+                                <Button variant="dark" onClick={this.hideColors}>Hide Colors</Button>
                             </Column>
                             <Column col xl="3" lg="3" md="3" sm="3">
                                 <h1>Red: {this.state.redScore}</h1>
