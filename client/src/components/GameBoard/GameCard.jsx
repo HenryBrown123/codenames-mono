3f6b0d252d896a4b03c5f0fd2ce84da255be6f9b
HenryBrown123 <henry.e.brown@icloud.com>
2022-11-17 17:10:58 +0000

    updated GameCard to use functional react component rather than Class

--- client/src/components/GameBoard/GameCard.jsx
+++ client/src/components/GameBoard/GameCard.jsx
@@ -3,29 +3,27 @@ import React , { Component } from 'react'
 import styled from 'styled-components'
 
 
-
 const Card = styled.div`
     margin: none;
     height: 100%;
-    background-color: ${props => props.backGroundColor || 'var(--color-card)'};
+    background-color: ${props => props.selectedColor || 'var(--color-card)'};
     .p {
         font-size: 5vh;
     }
 `
 
-class GameCard extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-        }
+const GameCard = (cardText, cardColor, cardSelected) => {
+    
+    // if not selected then backgroundColor will use default set in global CSS file
+    var selectedColor = null;
+    if (cardSelected){
+        selectedColor = cardColor;
     }
-    render() {
-        return (
-                <Card id="card">
-                    <p>GameCard</p>
-                </Card>
-        )
-    }
+    return (
+            <Card id="card" selectedColor={selectedColor} >
+                <p>{cardText}</p>
+            </Card>
+    )
 }
 
 export default GameCard
