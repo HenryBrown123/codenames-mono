687ff6a8f60028280798be90b1605e691e00a75f
HenryBrown123 <henry.e.brown@icloud.com>
2022-11-18 16:56:22 +0000

    Fixed export from components directory to include new components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,9 @@
 import Logo from './Logo'
 import NavBar from './Nav/NavBar'
+import Dashboard from './Dashboard/Dashboard'
+import GameBoard from './GameBoard/GameBoard'
+import GameCard from './GameBoard/GameCard'
+import LoadingSpinner from './LoadingSpinner/LoadingSpinner'
+import { ErrorMessage } from './ErrorMessage'
 
-export { Logo, NavBar }
+export { Logo, NavBar, Dashboard,GameBoard, LoadingSpinner, ErrorMessage, GameCard }
