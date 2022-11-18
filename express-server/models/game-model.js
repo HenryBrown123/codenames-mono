0677af4f006427d2dadc97f30e920e2cbd91af5d
HenryBrown123 <henry.e.brown@icloud.com>
2022-11-18 22:49:42 +0000

    Added extra parameters for number of cards, starting team and number of assasins

--- express-server/models/game-model.js
+++ express-server/models/game-model.js
@@ -15,11 +15,15 @@ var Game = new Schema({
         'default': shortid.generate
       },
 
+    /* game settings */
+    number_of_cards: {type: Number, required: true, default: 25} ,
+    starting_team: {type: String, required: true, default: 'green'},
+    number_of_assasins: {type: Number, required: true, default: 1},
+
     /* game states */
     game_paused: {type : Boolean, required:true, default:true},  
     round: {type: Number, required:true, default: 1},
     elapsed_round_time: {type: Number, required:true, default:0},
-    
     red_score: {type: Number, required: true, default: 0},
     green_score: {type: Number, required: true, default: 0},
     game_over: {type : Boolean, required:true, default:false},
