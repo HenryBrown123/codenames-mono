777ee9e87c568d8a0065d94c7d52140eb6bfa8ac
Henry Brown <henry.e.brown@icloud.com>
2024-10-04 11:35:44 +0100

    Cleaned up imports & exports

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,9 +1,7 @@
-import Logo from './Logo'
-import NavBar from './Nav/NavBar'
 import Dashboard from './Dashboard/Dashboard'
 import GameBoard from './GameBoard/GameBoard'
 import GameCard from './GameBoard/GameCard'
 import LoadingSpinner from './LoadingSpinner/LoadingSpinner'
-import { ErrorMessage } from './ErrorMessage'
+import ErrorMessage from './ErrorMessage/ErrorMessage'
 
-export { Logo, NavBar, Dashboard,GameBoard, LoadingSpinner, ErrorMessage, GameCard }
+export { Dashboard,GameBoard, LoadingSpinner, ErrorMessage, GameCard }
