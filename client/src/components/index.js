495efa44927b556a6a6be0b2f3fd3b3a62994ae1
Henry Brown <henry.e.brown@icloud.com>
2024-10-04 17:24:06 +0100

    removed old index.js after refactor

--- client/src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Dashboard from './Dashboard/Dashboard'
-import GameBoard from './GameBoard/GameBoard'
-import GameCard from './GameBoard/GameCard'
-import LoadingSpinner from './LoadingSpinner/LoadingSpinner'
-import ErrorMessage from './ErrorMessage/ErrorMessage'
-
-export { Dashboard,GameBoard, LoadingSpinner, ErrorMessage, GameCard }
