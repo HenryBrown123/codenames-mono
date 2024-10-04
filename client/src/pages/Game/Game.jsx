0c168af8f73073002d4e21e84d39c85b45afcffe
Henry Brown <henry.e.brown@icloud.com>
2024-10-04 11:40:41 +0100

    Added custom useQuery hook to top level game component which is provided to child components via the GameContextProvider.

--- client/src/pages/Game/Game.jsx
+++ client/src/pages/Game/Game.jsx
@@ -1,4 +1,5 @@
 import React, {  } from 'react'
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import { Dashboard, GameBoard, LoadingSpinner } from 'components'
 import { useGameData, GameContextProvider } from 'hooks'
 import styled from 'styled-components';
@@ -41,24 +42,40 @@ const GameContainer = styled.div`
 `;
 
 
+const queryClient = new QueryClient();
+
+const Game = () => {
+    return (
+        // Provide the client to your App
+       <QueryClientProvider client={queryClient}>
+                <CodeNamesGame />
+       </QueryClientProvider>
+      )
+}
+
+
 /**
  * Functional parent component containing all child components required by game.
  * This component fetches data from db via api call and passes into child components to present to the user.
  * 
  */
 
-const Game = () => {
-    // this is game data returned by api call
-    const { gameData } = useGameData(); 
+const CodeNamesGame = () => {
+    // this is a custom hook
+    const { data, error, isLoading } = useGameData()
 
-    if (!gameData) {
+    if (isLoading) {
         return (<LoadingSpinner displayText={"Loading a new game :)"}/>)
     } 
-    
+
+    if (error) {
+        return (<LoadingSpinner displayText={"Something went wrong :("}/>)
+    } 
+
     return(
         <Grid type="grid">
             <GameContainer type="game-container">
-                <GameContextProvider value = { gameData }>
+                <GameContextProvider value = { data[0].data.newgame }>
                     <GameBoardContainer type="main-section" >
                         <GameBoard/>
                     </GameBoardContainer>
