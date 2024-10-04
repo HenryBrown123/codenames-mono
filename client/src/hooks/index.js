567aa2be5ec2c356ba8aa9451b0986f222370a49
Henry Brown <henry.brown321@gmail.com>
2024-10-04 17:24:06 +0100

    removed old index.js after refactor

--- client/src/hooks/index.js
+++
@@ -1,2 +0,0 @@
-export * from './query/useGameData';
-export * from './useGameContext'
--- client/src/pages/Game/index.js
+++
@@ -1,6 +0,0 @@
-import Game from './Game'
-import ChangeGameContext from './Game'
-import { GameBoard } from '../../components/GameBoard'
-import { Dashboard } from '../../components/Dashboard'
-
-export {Game, ChangeGameContext, GameBoard, Dashboard}
