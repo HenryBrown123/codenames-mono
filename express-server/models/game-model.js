0e325686b9bd04128431b01173eeeffedeac06c9
Henry Brown <henry.e.brown@icloud.com>
2024-10-08 12:03:02 +0100

    Added virtual properties for game scores based on selected words

--- express-server/models/game-model.js
+++ express-server/models/game-model.js
@@ -1,40 +1,76 @@
 /** 
 * Game model object .. stores game states and words used within the game. 
 */
-import mongoose from 'mongoose'
-import shortid from 'shortid'
-import random from 'mongoose-simple-random'
+import mongoose from 'mongoose';
+import shortid from 'shortid';
 
 // this is the id used to join a game.. restricting to characters that are easy to read/type
 shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@');
 
 const Game = new mongoose.Schema({
-    _id: {
-        'type': String,
-        'default': shortid.generate
-      },
+  _id: {
+    type: String,
+    default: shortid.generate
+  },
+  /* game settings */
+  number_of_cards: {
+    type: Number,
+    required: true,
+    default: 24
+  },
+  starting_team: {
+    type: String,
+    required: true,
+    default: 'green'
+  },
+  number_of_assassins: {
+    type: Number,
+    required: true,
+    default: 1
+  },
+  /* game states */
+  game_paused: {
+    type: Boolean,
+    required: true,
+    default: true
+  },
+  game_over: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  /* json array of word objects */
+  words: [{
+    word: {
+      type: String,
+      required: true
+    },
+    color: {
+      type: String,
+      required: true
+    },
+    selected: {
+      type: Boolean,
+      required: true,
+      default: false
+    }
+  }]
+});
 
-    /* game settings */
-    number_of_cards: {type: Number, required: true, default: 24} ,
-    starting_team: {type: String, required: true, default: 'green'},
-    number_of_assasins: {type: Number, required: true, default: 1},
+/*
+ * virtuals added to derive the red_score and green_score based on the number of selected cards for each team
+ */
+Game.virtual('red_score').get(function() {
+  return this.words.filter(word => word.color === 'red' && word.selected).length;
+});
 
-    /* game states */
-    game_paused: {type : Boolean, required:true, default:true},  
-    round: {type: Number, required:true, default: 1},
-    elapsed_round_time: {type: Number, required:true, default:0},
-    red_score: {type: Number, required: true, default: 0},
-    green_score: {type: Number, required: true, default: 0},
-    game_over: {type : Boolean, required:true, default:false},
-    
 
-    /* json array of word objects */
-    words : [{
-        word: { type: String, required: true },
-        color: {type: String, required: true},
-        selected: {type: Boolean, required:true, default:false}
-    }]
+Game.virtual('green_score').get(function() {
+  return this.words.filter(word => word.color === 'green' && word.selected).length;
+});
 
-})
+// Ensure virtuals are included in JSON and object output
+Game.set('toJSON', { virtuals: true });
+Game.set('toObject', { virtuals: true });
 
-export default mongoose.model('game',Game);
+export default mongoose.model('Game', Game);
