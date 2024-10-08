846725132fbd23ff852f374836d716f0fa6b52c2
Henry Brown <henry.brown321@gmail.com>
2024-10-08 12:03:29 +0100

    removed nextTurn controller as not required

--- express-server/routes/game-router.js
+++ express-server/routes/game-router.js
@@ -1,11 +1,10 @@
 import express from 'express'
-import {getNewGame, getGame, nextTurn} from '../controllers/game-ctrl.js'
+import {getNewGame, getGame} from '../controllers/game-ctrl.js'
 
 
 const router = express.Router()
 
 router.get('/newGame/:gameSettings?', getNewGame)
 router.get('/getGame/:_id',  getGame)
-router.get('/nextTurn/:_id', nextTurn)
 
 export default router
