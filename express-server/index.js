3382a85a42120dd167614d471452ee4d5da1751c
Henry Brown <henry.e.brown@icloud.com>
2024-10-09 14:49:50 +0100

    Added swagger api docs to express application + neated up api paths

--- express-server/index.js
+++ express-server/index.js
@@ -1,24 +1,28 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import db from './db/index.js'
-import {default as wordRouter} from './routes/word-router.js'
-import {default as gameRouter} from './routes/game-router.js'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import db from "./db/index.js";
+import { default as wordRouter } from "./routes/word-router.js";
+import { default as gameRouter } from "./routes/game-router.js";
+import { specs, swaggerUi } from "./swagger.js";
 
-const app = express()
-const apiPort = 3000
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
+const app = express();
+const apiPort = 3000;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+/* Swagger - API Docs */
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
-app.use('/api', wordRouter);
-app.use('/api/game',gameRouter)
+/* api routes */
+app.use("/api", wordRouter);
+app.use("/api", gameRouter);
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
