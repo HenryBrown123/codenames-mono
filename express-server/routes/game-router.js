e8fa497ebdbabbb10fa81043df714940b235bfd3
Henry Brown <henry.e.brown@icloud.com>
2024-10-09 14:54:25 +0100

    Amended api paths for createing a new game (POST) (game/) and getGame (game/:_id) to conform to best practices.

--- express-server/routes/game-router.js
+++ express-server/routes/game-router.js
@@ -1,10 +1,138 @@
-import express from 'express'
-import {getNewGame, getGame} from '../controllers/game-ctrl.js'
+import express from "express";
+import { getGame, getNewGame } from "../controllers/game-ctrl.js";
 
+const router = express.Router();
 
-const router = express.Router()
+/**
+ * @swagger
+ * /games:
+ *   post:
+ *     summary: Creates a new game with specified or default settings.
+ *     tags:
+ *       - Gameplay
+ *     requestBody:
+ *       description: Optional settings for the new game
+ *       required: false
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               numberOfCards:
+ *                 type: integer
+ *               startingWithTeam:
+ *                 type: string
+ *               numberOfAssassins:
+ *                 type: integer
+ *           example:
+ *              numberOfCards: 25
+ *              startingWithTeam: 'green'
+ *              numberOfAssassins: 0
+ *     responses:
+ *       200:
+ *         description: New game created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 newgame:
+ *                   type: object
+ *                   properties:
+ *                     settings:
+ *                       type: object
+ *                       properties:
+ *                         numberOfCards:
+ *                           type: integer
+ *                         startingTeam:
+ *                           type: string
+ *                         numberOfAssassins:
+ *                           type: integer
+ *                     state:
+ *                       type: object
+ *                       properties:
+ *                         stage:
+ *                           type: string
+ *                         paused:
+ *                           type: boolean
+ *                         gameOver:
+ *                           type: boolean
+ *                         words:
+ *                           type: array
+ *                           items:
+ *                             type: object
+ *                             properties:
+ *                               selected:
+ *                                 type: boolean
+ *                               _id:
+ *                                 type: string
+ *                               word:
+ *                                 type: string
+ *                               color:
+ *                                 type: string
+ *                     _id:
+ *                       type: string
+ *                     rounds:
+ *                       type: array
+ *                       items:
+ *                         type: object
+ *                         properties:
+ *                           team:
+ *                             type: string
+ *                           codeword:
+ *                             type: string
+ *                           guesses_allowed:
+ *                             type: integer
+ *                           guessed_words:
+ *                             type: integer
+ *                     __v:
+ *                       type: integer
+ *                     red_score:
+ *                       type: integer
+ *                     green_score:
+ *                       type: integer
+ *                     id:
+ *                       type: string
+ *       500:
+ *         description: Internal server error
+ */
+router.post("/games", getNewGame);
 
-router.get('/newGame/:gameSettings?', getNewGame)
-router.get('/getGame/:_id',  getGame)
+/**
+ * @swagger
+ * /games/{_id}:
+ *   get:
+ *     summary: Returns an existing game as a JSON object.
+ *     tags:
+ *       - Gameplay
+ *     parameters:
+ *       - in: path
+ *         name: _id
+ *         required: true
+ *         description: The ID of the game to retrieve
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Game object
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                 game:
+ *                   type: object
+ *       400:
+ *         description: Invalid ID supplied
+ *       404:
+ *         description: Game not found
+ *       500:
+ *         description: Internal server error
+ */
+router.get("/games/:_id", getGame);
 
-export default router
+export default router;
