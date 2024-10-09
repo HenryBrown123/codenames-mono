25939cad8d364b10b398089acf07b7bbf05092c4
Henry Brown <henry.e.brown@icloud.com>
2024-10-09 14:50:43 +0100

    code formatting only

--- express-server/controllers/game-ctrl.js
+++ express-server/controllers/game-ctrl.js
@@ -1,60 +1,62 @@
-
-import Game from '../models/game-model.js'
-import { createNewGame } from '../services/gameService';
+import Game from "../models/game-model.js";
+import { createNewGame } from "../services/new-game-service.js";
 
 /**
- * Asynchronous function for returning a new game as a JSON object. 
- * 
- * @async 
+ * Asynchronous function for returning a new game as a JSON object.
+ *
+ * @async
  * @param req {Object} Request object
  * @param res {Object} New game JSON
  * @param req.params.gameSettings {Object} Optional param containing setting for the new game
- * 
+ *
  */
 
 export const getNewGame = async (req, res) => {
-  console.log('New game request received');
+  console.log("New game request received");
 
-  const gameSettings = req.params.gameSettings;
+  const gameSettings = req.body || {};
 
   try {
+    console.log("About to create new game...");
     const newGame = await createNewGame(gameSettings);
     console.log(newGame);
     res.status(200).json({ success: true, newgame: newGame });
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
-}
+  }
 };
 
 /**
- * Asynchronous function for returning an existing game as a JSON object. 
- * 
- * @async 
+ * Asynchronous function for returning an existing game as a JSON object.
+ *
+ * @async
  * @param req {Object} Request object
  * @param res {Object} New game JSON
  * @param req.params._id {String} game id for requested game
- * 
+ *
  */
 
+export const getGame = async (req, res) => {
+  const id = req.params._id;
 
-export const getGame = async (req,res) => {
-    const id = req.params._id
-    await Game.findById(id,function(err,results){
-        if(err){
-            return res.status(400).json({ success: false, error: err })
-        }
+  if (!id) {
+    return res
+      .status(400)
+      .json({ success: false, error: "ID parameter is missing" });
+  }
 
-        if (!results) {
-            return res
-                .status(404)
-                .json({ success: false, error: `game not found` })
-        }
+  try {
+    const game = await Game.findOne({ _id: id });
 
-        console.log(results)
-        return res.status(200).json({ success: true, game: results})       
-       })
-       .catch(err =>
-           console.log(err)
-       )
-}
+    if (!game) {
+      return res.status(404).json({ success: false, error: "Game not found" });
+    }
 
+    return res.status(200).json({ success: true, game });
+  } catch (error) {
+    console.error("Error fetching game:", error);
+    return res
+      .status(500)
+      .json({ success: false, error: "Internal server error" });
+  }
+};
