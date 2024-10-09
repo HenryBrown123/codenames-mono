bd30955b82ba8ff81da2d5c66b73449f18a26115
Henry Brown <henry.brown321@gmail.com>
2024-10-09 14:52:24 +0100

    Added more structure to Game model, parent settings and state object instead of flatter structure. Also added a "rounds" array to track each game round + moved the scoring into virtual parameters that are derived when the document is retrieved.

--- express-server/models/game-model.js
+++ express-server/models/game-model.js
@@ -1,76 +1,105 @@
-/** 
-* Game model object .. stores game states and words used within the game. 
-*/
-import mongoose from 'mongoose';
-import shortid from 'shortid';
+/**
+ * Game model object .. stores game states and words used within the game.
+ */
+import mongoose from "mongoose";
+import shortid from "shortid";
 
 // this is the id used to join a game.. restricting to characters that are easy to read/type
-shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@');
+shortid.characters(
+  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@",
+);
 
 const Game = new mongoose.Schema({
   _id: {
     type: String,
-    default: shortid.generate
+    default: shortid.generate,
   },
+
   /* game settings */
-  number_of_cards: {
-    type: Number,
-    required: true,
-    default: 24
-  },
-  starting_team: {
-    type: String,
-    required: true,
-    default: 'green'
-  },
-  number_of_assassins: {
-    type: Number,
-    required: true,
-    default: 1
-  },
-  /* game states */
-  game_paused: {
-    type: Boolean,
-    required: true,
-    default: true
-  },
-  game_over: {
-    type: Boolean,
-    required: true,
-    default: false
-  },
-  /* json array of word objects */
-  words: [{
-    word: {
-      type: String,
-      required: true
+  settings: {
+    numberOfCards: {
+      type: Number,
+      required: true,
+      default: 24,
     },
-    color: {
+    startingTeam: {
       type: String,
-      required: true
+      required: true,
+      default: "green",
     },
-    selected: {
+    numberOfAssassins: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+  },
+
+  /* current game state */
+
+  state: {
+    stage: { type: String, required: true, default: "none" },
+    paused: {
       type: Boolean,
       required: true,
-      default: false
-    }
-  }]
+      default: true,
+    },
+    gameOver: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+
+    codeword: { type: String },
+    guesses: { type: Number },
+
+    /* json array of word objects */
+    words: [
+      {
+        word: {
+          type: String,
+          required: true,
+        },
+        color: {
+          type: String,
+          required: true,
+        },
+        selected: {
+          type: Boolean,
+          required: true,
+          default: false,
+        },
+      },
+    ],
+  },
+
+  rounds: [
+    {
+      sequence: { type: Number },
+      team: { type: String },
+      codeword: { type: String },
+      guessesAllowed: { type: Number },
+      guessedWords: { type: Number },
+    },
+  ],
 });
 
 /*
  * virtuals added to derive the red_score and green_score based on the number of selected cards for each team
  */
-Game.virtual('red_score').get(function() {
-  return this.words.filter(word => word.color === 'red' && word.selected).length;
+Game.virtual("redScore").get(function () {
+  return this.state.words.filter(
+    (word) => word.color === "red" && word.selected,
+  ).length;
 });
 
-
-Game.virtual('green_score').get(function() {
-  return this.words.filter(word => word.color === 'green' && word.selected).length;
+Game.virtual("greenScore").get(function () {
+  return this.state.words.filter(
+    (word) => word.color === "green" && word.selected,
+  ).length;
 });
 
 // Ensure virtuals are included in JSON and object output
-Game.set('toJSON', { virtuals: true });
-Game.set('toObject', { virtuals: true });
+Game.set("toJSON", { virtuals: true });
+Game.set("toObject", { virtuals: true });
 
-export default mongoose.model('Game', Game);
+export default mongoose.model("Game", Game);
