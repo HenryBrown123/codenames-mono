e1bc8610077bd94c8d487af5cccab22f09977d6c
Henry Brown <henry.e.brown@icloud.com>
2024-10-21 23:43:42 +0100

    changed fetchCurrentStateFromDB to a placeholder function

--- express-server/services/gameplay/db-operations.js
+++ express-server/services/gameplay/db-operations.js
@@ -1,10 +1,8 @@
 export async function fetchCurrentStateFromDB(id) {
-    // Fetch current state from DB
-    return await Game.findById(id);
+  // Fetch current state from DB
+  return await Game.findById(id);
 }
 
-export async function fetchCurrentStateFromDB(id) {
-    // Fetch current state from DB
-    return await Game.findById(id);
+export async function updateGameState(id) {
+  // updates the game state with the new properties.
 }
-
