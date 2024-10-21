3a4e12dfe5bb1ac2cabbfec6d932c1f854961e59
Henry Brown <henry.brown321@gmail.com>
2024-10-21 23:42:47 +0100

    Added controller function for gameplay service

--- express-server/controllers/game-ctrl.js
+++ express-server/controllers/game-ctrl.js
@@ -1,5 +1,6 @@
 import Game from "../models/game-model.js";
 import { createNewGame } from "../services/new-game-service.js";
+import { executeTurn } from "../services/gameplay/service.js";
 
 /**
  * Asynchronous function for returning a new game as a JSON object.
@@ -55,6 +56,42 @@ export const getGame = async (req, res) => {
     return res.status(200).json({ success: true, game });
   } catch (error) {
     console.error("Error fetching game:", error);
+    return res
+      .status(500)
+      .json({ success: false, error: "Internal server error" });
+  }
+};
+
+/**
+ * Asynchronous function for processing a turn and returning the updated game object to display in front end.
+ *
+ * @async
+ * @param req {Object} Request object
+ * @param res {Object} New game JSON
+ * @param req.params._id {String} game id for requested game
+ *
+ */
+
+export const processTurn = async (req, res) => {
+  const id = req.params._id;
+  const updatedGameObject = req.body;
+
+  if (!id) {
+    return res
+      .status(400)
+      .json({ success: false, error: "ID parameter is missing" });
+  }
+
+  try {
+    const outputGameObject = await executeTurn(updatedGameObject);
+
+    if (!outputGameObject) {
+      return res.status(404).json({ success: false, error: "Game not found" });
+    }
+
+    return res.status(200).json({ success: true, game: outputGameObject });
+  } catch (error) {
+    console.error("Error processing gmae turn:", error);
     return res
       .status(500)
       .json({ success: false, error: "Internal server error" });
