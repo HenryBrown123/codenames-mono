af4c309abdefef2f005f6066678aa4b54243c76b
Henry Brown <henry.e.brown@icloud.com>
2024-10-21 23:42:25 +0100

    amended gameplay service to use inputGameObject instead of inputGameState so more clear that its the whole game object being passed in, instead of just the game state.

--- express-server/services/gameplay/service.js
+++ express-server/services/gameplay/service.js
@@ -1,33 +1,35 @@
-import { handleIntroStage, handleCodemasterStage, handleCodebreakerStage } from "./handlers";
+import {
+  handleIntroStage,
+  handleCodemasterStage,
+  handleCodebreakerStage,
+} from "./handlers.js";
 
 // object mapping each game stage to its handler
 const gameStageHandlers = {
-intro: handleIntroStage,
-codemaster: handleCodemasterStage,
-codebreaker: handleCodebreakerStage,
+  intro: handleIntroStage,
+  codemaster: handleCodemasterStage,
+  codebreaker: handleCodebreakerStage,
 };
 
 /**
- * Main function for executing gameplay logic depending on the input state. 
- * 
- * The game stage passed in as part of the inputState object determines which handler to run, which will then
- * validate the input state against the currently persisted state, before running the relavent game play logic 
+ * Main function for executing gameplay logic depending on the input state.
+ *
+ * The game stage passed in as part of the inputGameObject  determines which handler to run, which will then
+ * validate the input state against the previously persisted state, before running the relavent game play logic
  * and outputting a object representing the next state of the game to return via api (and display on ui)
- * 
- * @param {*} inputState 
- * @returns 
+ *
+ * @param {*} inputGameObject
+ * @returns
  */
 
-export async function executeTurn(inputState) {
-    const currentState = await fetchCurrentStateFromDB(inputState.id);
-    
-    const handleStage = gameStageHandlers[inputState.stage];
-    if (!handleStage) {
-      throw new Error('Invalid game stage');
-    }
-    try {
-      return await handleStage(inputState, currentState); // Validate and process in sequence
-    } catch (error) {
-      throw new Error(`Process turn failed: ${error.message}`);
-    }
+export async function executeTurn(inputGameObject) {
+  const handleStage = gameStageHandlers[inputGameObject.state.stage];
+  if (!handleStage) {
+    throw new Error("Invalid game stage");
+  }
+  try {
+    return await handleStage(inputGameObject); // Validate and process in sequence
+  } catch (error) {
+    throw new Error(`Process turn failed: ${error.message}`);
+  }
 }
