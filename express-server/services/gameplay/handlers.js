37e0f9956bf2b4fd52698c403b2ae509f5cdbfba
Henry Brown <henry.brown321@gmail.com>
2024-10-21 23:40:27 +0100

    changed input parameter from inputState -> inputGameObject so clear its the whole game object being passed in, not just the state.

--- express-server/services/gameplay/handlers.js
+++ express-server/services/gameplay/handlers.js
@@ -1,24 +1,33 @@
-import { validateIntroStage, validateCodemasterStage, validateCodebreakerStage } from './validation.js';
-import { processIntroStage, processCodemasterStage, processCodebreakerStage } from './processing.js';
+import {
+  validateIntroStage,
+  validateCodemasterStage,
+  validateCodebreakerStage,
+} from "./validation.js";
+
+import {
+  processIntroStage,
+  processCodemasterStage,
+  processCodebreakerStage,
+} from "./processing.js";
 
 /**
- * Validates and processes various game stages.
+ * Validates input state and processes various game stages.
  *
- * @param {Object} inputState - The current state of the game.
+ * @param {Object} inputGameObject - The current state of the game.
  * @returns {Object} - The updated game state after processing.
  * @throws {Error} - If validation fails or processing encounters an issue.
  */
-export function handleIntroStage(inputState) {
-    validateIntroStage(inputState);
-    return processIntroStage(inputState);
-  }
-  
-export function handleCodemasterStage(inputState) {
-    validateCodemasterStage(inputState);
-    return processCodemasterStage(inputState);
+export function handleIntroStage(inputGameObject) {
+  validateIntroStage(inputGameObject);
+  return processIntroStage(inputGameObject);
 }
 
-export function handleCodebreakerStage(inputState) {
-    validateCodebreakerStage(inputState);
-    return processCodebreakerStage(inputState);
+export function handleCodemasterStage(inputGameObject) {
+  validateCodemasterStage(inputGameObject);
+  return processCodemasterStage(inputGameObject);
+}
+
+export function handleCodebreakerStage(inputGameObject) {
+  validateCodebreakerStage(inputGameObject);
+  return processCodebreakerStage(inputGameObject);
 }
