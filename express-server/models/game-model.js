472c1545ad9b9caf28442e5f8c5cba90b13cd1bf
Henry Brown <henry.brown321@gmail.com>
2024-10-21 23:39:18 +0100

    updated game model so more readable & matches game processsing (removed redundant fields and renamed color -> team and word -> card)

--- express-server/models/game-model.js
+++ express-server/models/game-model.js
@@ -1,86 +1,107 @@
 import mongoose from "mongoose";
 import shortid from "shortid";
 
-shortid.characters("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@");
+// shortid used so identifier is more human readable & easier to type when joining a game
+shortid.characters(
+  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@"
+);
 
 // sub-schema for a game word
-const WordSchema = new mongoose.Schema({
-  word: { 
-    type: String, 
-    required: true,
+const CardSchema = new mongoose.Schema(
+  {
+    word: {
+      type: String,
+      required: true,
+    },
+    team: {
+      type: String,
+      required: true,
+      enum: ["red", "green", "none", "assassin"],
+    },
+    selected: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
-  color: { 
-    type: String, 
-    required: true, 
-    enum: ["red", "green", "blue"] 
-  },
-  selected: { 
-    type: Boolean, 
-    required: true, 
-    default: false 
-  },
-});
+  { _id: false }
+);
 
-// sub-schema for each round 
-const RoundSchema = new mongoose.Schema({
-  sequence: { type: Number },
-  roundStartedAt: { type: Date },
-  team: { type: String },
-  codeword: { type: String },
-  guessesAllowed: { 
-    type: Number,
+// sub-schema for each round
+const RoundSchema = new mongoose.Schema(
+  {
+    team: { type: String },
+    codeword: { type: String },
+    guessesAllowed: {
+      type: Number,
+    },
+    guessedWords: [String],
   },
-  guessedWords: [String],
-});
+  { _id: false }
+);
 
-// sub-schema for settings 
-const SettingsSchema = new mongoose.Schema({
-  numberOfCards: {
-    type: Number,
-    required: true,
-    default: 24,
+// sub-schema for settings
+const SettingsSchema = new mongoose.Schema(
+  {
+    numberOfCards: {
+      type: Number,
+      required: true,
+      default: 24,
+    },
+    startingTeam: {
+      type: String,
+      required: true,
+      default: "green",
+      enum: ["green", "red"],
+    },
+    numberOfAssassins: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
   },
-  startingTeam: {
-    type: String,
-    required: true,
-    default: "green",
-    enum: ["green", "red"],
-  },
-  numberOfAssassins: {
-    type: Number,
-    required: true,
-    default: 1,
-  },
-});
+  { _id: false }
+);
 
 // sub-schema for game state (stuff you need to know to track gameplay)
-const GameStateSchema = new mongoose.Schema({
-  stage: { 
-    type: String, 
-    required: true, 
-    default: "none",
-    enum: ["none", "intro", "codemaster","codebreaker","gameover"],
+const GameStateSchema = new mongoose.Schema(
+  {
+    stage: {
+      type: String,
+      required: true,
+      default: "intro",
+      enum: ["intro", "codemaster", "codebreaker", "gameover"],
+    },
+    winner: { type: String, required: false },
+    cards: [CardSchema],
+    rounds: [RoundSchema],
   },
-  words: [WordSchema],
-  rounds: [RoundSchema],
-});
+  { _id: false }
+);
 
-const GameSchema = new mongoose.Schema({
-  _id: {
-    type: String,
-    default: shortid.generate,
+const GameSchema = new mongoose.Schema(
+  {
+    _id: {
+      type: String,
+      default: shortid.generate,
+    },
+    state: GameStateSchema,
+    settings: SettingsSchema,
   },
-  settings: SettingsSchema,
-  state: GameStateSchema,
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 // Virtuals for derived scores
 GameSchema.virtual("redScore").get(function () {
-  return this.state.words.filter(word => word.color === "red" && word.selected).length;
+  return this.state.cards.filter(
+    (word) => word.color === "red" && word.selected
+  ).length;
 });
 
 GameSchema.virtual("greenScore").get(function () {
-  return this.state.words.filter(word => word.color === "green" && word.selected).length;
+  return this.state.cards.filter(
+    (word) => word.color === "green" && word.selected
+  ).length;
 });
 
 // Ensure virtuals are included in JSON and object output
