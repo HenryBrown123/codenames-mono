38d1870ad3880a55292d2c90913f9854005ad0c2
Henry Brown <henry.e.brown@icloud.com>
2024-10-25 15:34:01 +0100

    added common constants and types folder for sharing amongst game feature.

---
+++ express-server/game/game-common-constants.ts
@@ -0,0 +1,13 @@
+export const TEAM = {
+  RED: "red",
+  GREEN: "green",
+  NONE: "none",
+  ASSASSIN: "assassin",
+} as const;
+
+export const STAGE = {
+  INTRO: "intro",
+  CODEMASTER: "codemaster",
+  CODEBREAKER: "codebreaker",
+  GAMEOVER: "gameover",
+} as const;
