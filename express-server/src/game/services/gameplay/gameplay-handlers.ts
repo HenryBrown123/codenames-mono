52396c38e748e22758c9baf5db17798809844896
Henry Brown <henry.brown321@gmail.com>
2024-11-18 19:07:57 +0000

    formatting changes to gameplay-handlers

--- express-server/src/game/services/gameplay/gameplay-handlers.ts
+++ express-server/src/game/services/gameplay/gameplay-handlers.ts
@@ -12,27 +12,56 @@ import {
 import { GameState } from "src/game/game-common-types";
 
 /**
- * Validates input state and processes various game stages.
+ * Handles the intro stage: validates, processes, and returns updated game state.
  *
- * @param {GameState} inputGameState - The current state of the game.
- * @returns {GameState} - The updated game state after processing.
+ * @param {string} gameId - The ID of the game to update.
+ * @param {GameState} inputGameState - The current state of the game provided by the client.
+ * @returns {Promise<GameState>} - The updated game state after processing.
  * @throws {Error} - If validation fails or processing encounters an issue.
  */
-export function handleIntroStage(inputGameState: GameState): GameState {
+export async function handleIntroStage(
+  inputGameState: GameState
+): Promise<GameState> {
   validateIntroStage(inputGameState);
   return processIntroStage(inputGameState);
 }
 
-export function handleCodemasterStage(inputGameState: GameState): GameState {
+/**
+ * Handles the codemaster stage: validates, processes, and returns updated game state.
+ *
+ * @param {string} gameId - The ID of the game to update.
+ * @param {GameState} inputGameState - The current state of the game provided by the client.
+ * @returns {Promise<GameState>} - The updated game state after processing.
+ * @throws {Error} - If validation fails or processing encounters an issue.
+ */
+export async function handleCodemasterStage(
+  inputGameState: GameState
+): Promise<GameState> {
   validateCodemasterStage(inputGameState);
   return processCodemasterStage(inputGameState);
 }
 
-export function handleCodebreakerStage(inputGameState: GameState): GameState {
+/**
+ * Handles the codebreaker stage: validates, processes, and returns updated game state.
+ *
+ * @param {string} gameId - The ID of the game to update.
+ * @param {GameState} inputGameState - The current state of the game provided by the client.
+ * @returns {Promise<GameState>} - The updated game state after processing.
+ * @throws {Error} - If validation fails or processing encounters an issue.
+ */
+export async function handleCodebreakerStage(
+  inputGameState: GameState
+): Promise<GameState> {
   validateCodebreakerStage(inputGameState);
   return processCodebreakerStage(inputGameState);
 }
 
+/**
+ * Handles the game over stage. Throws an error as no more turns are allowed.
+ *
+ * @param {GameState} inputGameState - The current state of the game.
+ * @throws {Error} - Always throws an error as the game is over.
+ */
 export function handleGameOverStage(inputGameState: GameState): never {
   throw new Error("Game has finished. No more turns.");
 }
