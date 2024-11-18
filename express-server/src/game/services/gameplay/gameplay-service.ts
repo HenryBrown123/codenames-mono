923ad68d9c53a42cb8ee15b8b802468509ab9314
Henry Brown <henry.e.brown@icloud.com>
2024-11-18 19:09:05 +0000

    updated gameplay service to remove teams from cards if not codemaster stage. Teams are added back in from db prior to executing handlers.

--- express-server/src/game/services/gameplay/gameplay-service.ts
+++ express-server/src/game/services/gameplay/gameplay-service.ts
@@ -4,12 +4,15 @@ import {
   handleCodebreakerStage,
   handleGameOverStage,
 } from "./gameplay-handlers";
-import { GameState, Stage } from "@game/game-common-types";
-import { updateGameDocument } from "./db-operations";
+import { GameState, Card, Stage } from "@game/game-common-types";
+import { STAGE } from "@game/game-common-constants";
+import { fetchGameDocument, updateGameDocument } from "./db-operations";
 
-// defines the function type of handlers. Must accept a GameState and return a GameState
+// defines the function type of handlers. Must accept a GameState and return a Promise<GameState>
 // unless an error is thrown...
-type GameStageHandler = (inputGameState: GameState) => GameState;
+type GameStageHandler = (
+  inputGameState: GameState
+) => Promise<GameState> | never;
 
 // Game stage handler lookup object. All game stages must feature
 const gameStageHandlers: Record<Stage, GameStageHandler> = {
@@ -28,29 +31,80 @@ const gameStageHandlers: Record<Stage, GameStageHandler> = {
  * to return via the API (and display on UI).
  *
  * @param {string} gameId - The ID of the game to update.
- * @param {GameState} inputGameObject - The current state of the game.
+ * @param {GameState} inputGameState - The current state of the game.
  * @returns {Promise<GameState>} - The updated game state after processing.
  * @throws {Error} - If validation fails or processing encounters an issue.
  */
 export async function executeTurn(
   gameId: string,
-  inputGameObject: GameState
+  inputGameState: GameState
 ): Promise<GameState> {
-  const handleStage = gameStageHandlers[inputGameObject.stage];
+  const handleStage = gameStageHandlers[inputGameState.stage];
   if (!handleStage) {
     console.log("executeTurn: invalid game stage");
     throw new Error("Invalid game stage");
   }
   try {
-    const updatedGameState = handleStage(inputGameObject); // Validate and process in sequence
+    // Fetch the persisted game state and hydraft the input game state with card teams
+    const originalGameState = await fetchGameDocument(gameId);
+    const hydratedGameState = addTeamPropToCard(
+      inputGameState,
+      originalGameState.state.cards
+    );
+
+    // Process the hydrated game state & persist to db
+    const updatedGameState = await handleStage(hydratedGameState);
     console.log(
       "executeTurn: updated game state to persist: ",
       updatedGameState
     );
-    await updateGameDocument(gameId, updatedGameState); // Persist the updated game state
+    await updateGameDocument(gameId, updatedGameState);
+
+    // if not a codemaster stage then sanitise game state to remove teams from unselected cards
+    if (updatedGameState.stage !== STAGE.CODEMASTER) {
+      return sanitizeGameStateForClient(updatedGameState);
+    }
+
     return updatedGameState;
   } catch (error: any) {
-    console.log("executeTurn: failed ", error.message);
+    console.log("executeTurn failed: ", error.message);
     throw new Error(`Process turn failed: ${error.message}`);
   }
+}
+
+/**
+ * Adds the team property to each card in the input game state based on the original game state's cards.
+ *
+ * @param {GameState} inputGameState - The current state of the game provided by the client.
+ * @param {Array<Card>} originalCards - The original cards array from the game state (from the database).
+ * @returns {GameState} - The updated input game state with team properties added from the original game state.
+ */
+function addTeamPropToCard(
+  inputGameState: GameState,
+  originalCards: Card[]
+): GameState {
+  return {
+    ...inputGameState,
+    cards: inputGameState.cards.map((card, index) => ({
+      ...card,
+      team: originalCards[index].team,
+    })),
+  };
+}
+
+/**
+ * Utility function to sanitize the game state for client consumption.
+ * Removes team information from unselected cards to prevent cheating.
+ *
+ * @param {GameState} gameState - The game state to sanitize.
+ * @returns {GameState} - The sanitized game state.
+ */
+function sanitizeGameStateForClient(gameState: GameState): GameState {
+  return {
+    ...gameState,
+    cards: gameState.cards.map((card: Card) => ({
+      ...card,
+      team: card.selected ? card.team : undefined, // Remove team color if not selected
+    })),
+  };
 }
