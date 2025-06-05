6ca677ed478ab77522250e4b55fb1bce3a3d2124
Henry Brown <henry.e.brown@icloud.com>
2025-06-05 20:56:22 +0100

    feature: added role assignment sub feature.

--- backend/src/common/db/db.mongoose.legacy.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect("mongodb://127.0.0.1:27017/codenames").catch((e) => {
-  console.error("Connection error", e.message);
-});
-
-export const mongoDb = mongoose.connection;
