7821b2f03f2e1b5d7014f5e6b27a0cd807237857
Henry Brown <henry.brown321@gmail.com>
2025-07-18 02:49:51 +0100

    fix: removed style exports from index.ts

--- frontend/src/gameplay/ui-components/cards/index.ts
+++ frontend/src/gameplay/ui-components/cards/index.ts
@@ -1,4 +1,3 @@
-export { GameCard } from './game-card';
-export { CardVisibilityProvider } from './card-visibility-provider';
-export { CardContainer, BaseCard, CardOverlay, CardWord } from './card-styles';
-export * from './card-utils';
+export { GameCard } from "./game-card";
+export { CardVisibilityProvider } from "./card-visibility-provider";
+export * from "./card-utils";
